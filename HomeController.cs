1b12963712fff68c14157230c42711f232edf689
Claudio felix <claudio.felixdossantos@yahoo.com.br>
2020-07-15 17:19:46 -0300

    MVC 5 com razor, View Razor cshtml, ARROBA mvc 5, ViewBag, DateTime, DateTime.Now.Hour

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
+
+//O método Index acima usa um modelo de exibição para gerar uma resposta HTML para o navegador.
+// Os métodos do controlador(também conhecidos como métodos de ação), como o método Index acima, 
+//  geralmente retornam um ActionResult(ou uma classe derivada de ActionResult), não tipos primitivos como cadeia de caracteres.
---
+++ RazorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Razor.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Index()
+        {
+            ViewBag.curso = "Curso de AspNet MVC 5 com Razor";  // dicionário ViewBag
+
+            var hora = DateTime.Now.Hour; // obtem o componente hora
+
+            ViewBag.hora = hora;
+
+            
+            if (hora > 12)
+            {
+               ViewBag.turno = "tarde";
+            }
+            else
+            {
+                ViewBag.turno = "manhã";
+            }
+
+            return View();
+        }
+    }
+}
